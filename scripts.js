0668d95d79fd28a70a99e261a64e69ead6e1afd8
Al Harvey <alharvey789@gmail.com>
2019-02-18 19:05:42 -0600

    Created greeter function for home page

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
-var nameChecker = function(nameChecker){
-    var name = prompt('Please enter your name');
+var name;
 
+var blankCheck = function blankCheck(){
     if(name === ''){
         name = prompt('TELL ME YOUR NAME!!!!');
+
+        blankCheck();
     }
-    else{
-        document.querySelector('#header h1').innerHTML = `Welcome To My Portfolio ${name}`;
-    }
+};
+
+var nameChecker = function nameChecker(){
+    name = prompt('Please enter your name');
+
+    blankCheck();
+
+    document.querySelector('#header h1').innerHTML = `Welcome To My Portfolio ${name}`;
 };
 
 nameChecker();
