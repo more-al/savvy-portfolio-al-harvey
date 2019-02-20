8cd8290e39c4f8b4d88042f891c584bbb8fbf7cb
Al Harvey <alharvey789@gmail.com>
2019-02-19 18:19:41 -0600

    :sparkles: Connected new JS files to index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import Content from './src/Content';
+import Footer from './src/Footer';
+import Header from './src/Header';
 import Navigation from './src/Navigation';
 
 var originalContent = document.body.innerHTML;
@@ -7,22 +10,26 @@ var name;
 var blankCheck = function blankCheck(){
     if(name === ''){
         name = prompt('TELL ME YOUR NAME!!!!');
-        
+
         blankCheck();
     }
 };
 
 var nameChecker = function nameChecker(){
     name = prompt('Please enter your name');
-    
+
     blankCheck();
-    
+
     document.querySelector('#header h1').innerHTML = `Welcome To My Portfolio ${name}`;
 };
 
-nameChecker();
 
 document.body.innerHTML = ` 
-    ${Navigation}
-    ${originalContent}
+${Navigation}
+${Header}
+${Content}
+${Footer}
+${originalContent}
 `;
+
+nameChecker();
--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,11 @@
 export default `
-<div id="footer">
-<li class="white">
-    <a href="https://www.linkedin.com/in/al-harvey-jr-18321910a/"><i class="fab fa-linkedin"></i>LinkedIn</a>
-</li>
+    <div id="footer">
+    <li class="white">
+        <a href="https://www.linkedin.com/in/al-harvey-jr-18321910a/"><i class="fab fa-linkedin"></i>LinkedIn</a>
+    </li>
 
-<li class="white">
-    <a href="https://github.com/more-al"><i class="fab fa-github"></i> GitHub</a>
-</li>
-</div>
+    <li class="white">
+        <a href="https://github.com/more-al"><i class="fab fa-github"></i> GitHub</a>
+    </li>
+    </div>
 `;
