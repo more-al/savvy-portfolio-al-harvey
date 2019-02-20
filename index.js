19c48a287d33d4951a17914af4a8321031ddeffd
Al Harvey <alharvey789@gmail.com>
2019-02-19 19:40:47 -0600

    :hammer: Refactor root element from body to dedicated div

--- index.js
+++ index.js
@@ -2,34 +2,16 @@ import Content from './src/Content';
 import Footer from './src/Footer';
 import Header from './src/Header';
 import Navigation from './src/Navigation';
-
-var originalContent = document.body.innerHTML;
-
-var name;
-
-var blankCheck = function blankCheck(){
-    if(name === ''){
-        name = prompt('TELL ME YOUR NAME!!!!');
-
-        blankCheck();
-    }
-};
-
-var nameChecker = function nameChecker(){
-    name = prompt('Please enter your name');
-
-    blankCheck();
-
-    document.querySelector('#header h1').innerHTML = `Welcome To My Portfolio ${name}`;
-};
+// import nameChecker from './src/Greeter';
 
 
-document.body.innerHTML = ` 
-${Navigation}
-${Header}
-${Content}
-${Footer}
-${originalContent}
+var root = document.querySelector('#root');
+
+root.innerHTML = ` 
+    ${Navigation}
+    ${Header}
+    ${Content}
+    ${Footer}
 `;
 
-nameChecker();
+// nameChecker();
