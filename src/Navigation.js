da11e33d69f91298fd8ec996ca625916aeb2edfc
Al Harvey <alharvey789@gmail.com>
2019-03-05 18:54:50 -0600

    🔧 Fix home -> Home mismatch

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,27 @@
+import { lowerCase } from 'lodash';
+
 function buildLinks(links){
     var i = 0;
     var linkList = '';
+    var destination = '';
 
     while(i < links.length){
-        linkList += `<li><a data-navigo href="./${links[i]}">${links[i]}</a></li>`;
+        if(links[i] !== 'Home'){
+            destination = lowerCase(links[i]);
+        }
+
+        linkList += `
+        <li>
+        <a data-navigo href="./${destination}">
+        ${links[i]}
+        </a>
+        </li>
+        `;
+
+
         i++;
     }
-    
+
     return linkList;
 }
 
